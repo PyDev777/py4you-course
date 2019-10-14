fdb477733e19d075f8b005f0c15bf7ec3524f987
pydev777 <pydev777@gmail.com>
2019-10-14 17:47:31 +0300

    Create models: Essay, Catalog, Tag

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Essay(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+
+    description = models.TextField()
+    published = models.DateField()
+
+    # img = models.ImageField()
+    # img_source = models.URLField(max_length=255)
+    # author = models.CharField(max_length=255)
+
+    essay_source = models.URLField(max_length=255)
+    parsing_date = models.DateTimeField(auto_created=True)
+
+    cat = models.ManyToManyField(Category)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
