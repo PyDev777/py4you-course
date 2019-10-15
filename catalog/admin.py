8748f7f665665238c292bdc459e059eeb04fd094
pydev777 <pydev777@gmail.com>
2019-10-15 15:56:29 +0300

    Add tables to Admin, Add Category Crawler

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+
+from catalog.models import Essay, Category, Tag
+
+
+class EssayAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Essay, EssayAdmin)
+admin.site.register(Tag)
--- catalog/management/commands/get_essays.py
+++ catalog/management/commands/get_essays.py
@@ -1,8 +1,14 @@
 from threading import Thread
 from requests_html import HTMLSession
 from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+from datetime import datetime
 
 from catalog.models import *
+
+# https://freeessays.page/category-sitemap.xml
+# https://freeessays.page/post_tag-sitemap.xml
+# https://freeessays.page/post-sitemap1.xml
 
 
 def crawler(url):
@@ -11,26 +17,27 @@ def crawler(url):
         response = session.get(url)
 
     name = response.html.xpath('//h1')[0].text
-    print('description: ', name)
-    description = response.html.xpath('//div[@class="entry-content"]')
-    print('description: ', description)
-    # description = response.html.xpath('//div[@id="description"]/span[2]')[0].text
+
+    description = response.html.xpath('//div[contains(@class,"entry-content")]')[0].text
+    description = '\n'.join([f'<p>{p}</p>' for p in description.split('\n') if p])
+
+    # published = response.html.xpath('//div[@class="entry-date"]/time/@datetime')[0]  # 2019-09-03T09:08:48+00:00
+    published = response.html.xpath('//div[@class="entry-date"]/time')[0].text
+    published = datetime.strptime(published, '%B %d, %Y')
+
     # img_source = response.html.xpath('//img[@id="coverImage"]/@src')[0]
+    # parsing_date = ''
 
-    slug = ''
-    published = ''
-    essay_source = ''
-    parsing_date = ''
     cat = ''
-    char = ''
-
-
+    tag = ''
 
     essay = {
         'name': name,
+        'slug': slugify(name),
         # 'img_source': img_source,
         'essay_source': url,
-        'description': description
+        'description': description,
+        'published': published,
     }
 
     print(essay)
@@ -42,6 +49,6 @@ class Command(BaseCommand):
     help = 'Running books scraper'
 
     def handle(self, *args, **options):
-        url = 'https://freeessays.page/argumentative-essay-statement-in-opposition-to-distant-learning/'
+        url = 'https://freeessays.page/why-marriage-is-still-important/'
         Thread(target=crawler, args=(url, )).start()
         print('Done!')
