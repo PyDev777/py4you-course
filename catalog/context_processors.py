82da8a7385ec1ebe22f2ce709ce8b665503a1b1e
pydev777 <pydev777@gmail.com>
2019-10-22 19:13:50 +0300

    Add Cache to context_processors, remove from Header

--- catalog/context_processors.py
+++ catalog/context_processors.py
@@ -1,6 +1,8 @@
 from .models import Category, Tag
+from cache_memoize import cache_memoize
 
 
+@cache_memoize(60*5)
 def menu(request):
     cats = Category.objects.all().order_by('name')
     tags = Tag.objects.all().order_by('name')
